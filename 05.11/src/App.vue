f6edf7c0fe93a79b0ae9b6937865ac0885d096cd
karataevD <73880650+karataevD@users.noreply.github.com>
2020-11-17 17:18:48 +0600

    Создание страницы входа в приложение

--- 05.11/src/App.vue
+++ 05.11/src/App.vue
@@ -1,9 +1,11 @@
 <template>
 <div class="container">
+  <img class="draftLogo" src="./assets/draftLogo.svg">
   <form v-show="step === 1">
     <div class="form-group">
-      <label for="login">Введите вашу электронную почту</label>
-      <input type="email" class="form-control" id="login">
+      <label for="login">Электронная почта</label>
+      <input type="email" class="form-control" id="login" placeholder="example@example.org">
+      <small id="emailHelp" class="form-text text-muted">Мы никогда никому не передадим вашу электронную почту.</small>
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Пароль</label>
@@ -14,10 +16,10 @@
       <label class="form-check-label" for="checkBox1">Запомнить меня</label>
     </div>
     <div class="btnSignIn">
-      <button type="send" class="btn btn-primary">Вход</button>
+      <button type="send" class="btn btn-warning">Вход</button>
     </div>
     <div class="btnRegister">
-      <button @click="nextStep" type="button" class="btn btn-primary">Регистрация</button>
+      <button @click="nextStep" type="button" class="btn btn-warning">Регистрация</button>
     </div>
   </form>
   <form v-show="step === 2">
@@ -31,16 +33,17 @@
     </div>
     <div class="form-group">
       <label for="login">Введите вашу электронную почту</label>
-      <input type="email" class="form-control" id="login">
+      <input type="email" class="form-control" id="login" placeholder="example@example.org">
+      <small id="emailHelp" class="form-text text-muted">Мы никогда никому не передадим вашу электронную почту.</small>
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Придумайте пароль</label>
       <input type="password" class="form-control" id="password">
     </div>
     <div class="btnSignIn">
-      <button @click="backStep" type="button" class="btn btn-secondary">Назад</button>
+      <button @click="backStep" type="button" class="btn btn-light">Назад</button>
       <div class="btnInRegistration">
-      <button type="send" class="btn btn-primary">Зарегистрироваться</button>
+      <button type="send" class="btn btn-warning">Зарегистрироваться</button>
       </div>
     </div>
   </form>
@@ -68,7 +71,7 @@
 <style>
 .container{
   position: absolute;
-  top: 20%;
+  top: 10%;
   width: 20%;
   left: 40%;
 }
