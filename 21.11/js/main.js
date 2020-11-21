d4bec376ee171945c41853d703e23eb6854dd1fb
karataevD <73880650+karataevD@users.noreply.github.com>
2020-11-21 23:01:55 +0600

    Условия, списки, циклы

---
+++ 21.11/js/main.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    data: {
+        show: true,
+        cars: [
+            {model: "BMW", speed: 250.8},
+            {model: "Audi", speed: 240.21},
+            {model: "Mercedes-Benz AMG", speed: 350.4},
+            {model: "Ford", speed: 160.5},           
+        ]
+    },
+    methods: {
+
+    }
+})
